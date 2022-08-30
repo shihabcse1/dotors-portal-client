e8319fc24e6c53ee77169dc4daea75baf8507aa9
shihabcse1 <tomdshihab@gmail.com>
2022-08-30 13:56:56 +0600

    Make Appointment added

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MakeAppointment = () => {
+    return (
+        <section>
+            <div></div>
+            <div></div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
---
+++ src/Pages/Home/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Testimonials = () => {
+    return (
+        <section>
+            <div></div>
+            <div></div>
+        </section>
+    );
+};
+
+export default Testimonials;
