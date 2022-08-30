d3d591c326b818490c0723d490d7e9932a0c7a23
shihabcse1 <tomdshihab@gmail.com>
2022-08-30 12:04:28 +0600

    InfoCard part added

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle, bgClass }) => {
     return (
         <div>
-            <div class="card lg:card-side bg-base-100 shadow-xl bg-accent">
-                <figure>
+            <div class={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+                <figure className="pl-5">
                     <img src={img} alt="Album" />
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">New album is released!</h2>
+                <div class="card-body text-white">
+                    <h2 class="card-title">{cardTitle}</h2>
                     <p>Click the button to listen on Spotiwhy app.</p>
                     <div class="card-actions justify-end">
                         <button class="btn btn-primary">Listen</button>
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Services = () => {
+    return (
+        <div className="my-28">
+            <div className="text-center">
+                <h3 className="text-primary text-xl font-bold uppercase">
+                    Our Services
+                </h3>
+                <h2>Services We Provide</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
