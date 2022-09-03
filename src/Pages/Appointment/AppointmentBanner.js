25e6e8da37f75500911c0b3fac078943b4f1940a
shihabcse1 <tomdshihab@gmail.com>
2022-09-03 09:53:51 +0600

    Appointment header added initially

---
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,31 @@
+import React from "react";
+import chair from "../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import { format } from "date-fns";
+import "react-day-picker/dist/style.css";
+import { useState } from "react";
+
+const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date());
+    return (
+        <div class="hero min-h-screen">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src={chair}
+                    className="max-w-sm rounded-lg shadow-2xl"
+                    alt="Dentist Chair"
+                />
+                <div>
+                    <DayPicker
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                    />
+                    <p>You picked {format(date, "PP")}.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AppointmentBanner;
