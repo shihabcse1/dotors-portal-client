a6bb9e3f42848f7688e02c385500b0de0f06edd7
shihabcse1 <tomdshihab@gmail.com>
2022-09-04 13:12:56 +0600

    Service data loaded successfully from json file

--- src/Pages/Appointment/AvailiableAppointments.js
+++ src/Pages/Appointment/AvailiableAppointments.js
@@ -1,13 +1,15 @@
+import { data } from "autoprefixer";
 import { format } from "date-fns";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import Service from "./Service";
 
 const AvailiableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
-        fetch("service.json")
+        fetch("services.json")
             .then((res) => res.json())
             .then((data) => setServices(data));
     }, []);
@@ -17,6 +19,11 @@ const AvailiableAppointments = ({ date }) => {
             <h4 className="text-xl text-secondary text-center">
                 Available Appointments on {format(date, "PP")}
             </h4>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {services.map((service) => (
+                    <Service key={service._id} service={service}></Service>
+                ))}
+            </div>
         </div>
     );
 };
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, slots } = service;
+
     return (
-        <div>
-            
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title text-secondary">{name}</h2>
+                <p>
+                    {slots.length > 0 ? (
+                        <span>{slots[0]}</span>
+                    ) : (
+                        <span className="text-red-500">Try another date</span>
+                    )}
+                </p>
+                <p>
+                    {slots.length}
+                    {slots.length > 1 ? " spaces" : " space"} available
+                </p>
+                <div class="card-actions justify-center">
+                    <button
+                        disabled={slots.length === 0}
+                        class="btn btn-primary"
+                    >
+                        Make Appointment
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
