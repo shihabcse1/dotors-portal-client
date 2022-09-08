acdcdd2d79f5cce58a2479a939848a8c16829198
shihabcse1 <tomdshihab@gmail.com>
2022-09-08 12:53:59 +0600

    Margin fixed initially of AvailibleAppointment page

--- src/Pages/Appointment/AvailiableAppointments.js
+++ src/Pages/Appointment/AvailiableAppointments.js
@@ -18,7 +18,7 @@ const AvailiableAppointments = ({ date }) => {
 
     return (
         <div>
-            <h4 className="text-xl text-secondary text-center">
+            <h4 className="text-xl text-secondary text-center my-12">
                 Available Appointments on {format(date, "PP")}
             </h4>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
