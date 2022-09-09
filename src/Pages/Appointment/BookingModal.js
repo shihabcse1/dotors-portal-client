5cfa9f3239e7ce5082db3d7a2608bbe104339801
shihabcse1 <tomdshihab@gmail.com>
2022-09-09 15:48:25 +0600

    Singin with Google code added

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -13,16 +13,20 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
 
     return (
         <div>
-            <input type="checkbox" id="booking-modal" class="modal-toggle" />
-            <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-box">
+            <input
+                type="checkbox"
+                id="booking-modal"
+                className="modal-toggle"
+            />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
                     <label
                         for="booking-modal"
-                        class="btn btn-sm btn-circle absolute right-2 top-2"
+                        className="btn btn-sm btn-circle absolute right-2 top-2"
                     >
                         ✕
                     </label>
-                    <h3 class="font-bold text-lg text-secondary">
+                    <h3 className="font-bold text-lg text-secondary">
                         Booking for {name}
                     </h3>
                     <form
@@ -33,11 +37,11 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                             type="text"
                             disabled
                             value={format(date, "PP")}
-                            class="input input-bordered w-full max-w-xs"
+                            className="input input-bordered w-full max-w-xs"
                         />
                         <select
                             name="slot"
-                            class="select select-bordered w-full max-w-xs"
+                            className="select select-bordered w-full max-w-xs"
                         >
                             {slots.map((slot) => (
                                 <option value={slot}>{slot}</option>
@@ -47,24 +51,24 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                             type="text"
                             name="name"
                             placeholder="Your Name"
-                            class="input input-bordered w-full max-w-xs"
+                            className="input input-bordered w-full max-w-xs"
                         />
                         <input
                             type="text"
                             name="email"
                             placeholder="Email Address"
-                            class="input input-bordered w-full max-w-xs"
+                            className="input input-bordered w-full max-w-xs"
                         />
                         <input
                             type="text"
                             name="phone"
                             placeholder="Phone Number"
-                            class="input input-bordered w-full max-w-xs"
+                            className="input input-bordered w-full max-w-xs"
                         />
                         <input
                             type="submit"
                             value="submit"
-                            class="btn btn-secondary w-full max-w-xs"
+                            className="btn btn-secondary w-full max-w-xs"
                         />
                     </form>
                 </div>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,9 +4,9 @@ const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
 
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body text-center">
-                <h2 class="text-xl font-bold text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {slots.length > 0 ? (
                         <span>{slots[0]}</span>
@@ -18,12 +18,12 @@ const Service = ({ service, setTreatment }) => {
                     {slots.length}
                     {slots.length > 1 ? " spaces" : " space"} available
                 </p>
-                <div class="card-actions justify-center">
+                <div className="card-actions justify-center">
                     <label
                         for="booking-modal"
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
-                        class="btn btn-sm btn-secondary text-white uppercase"
+                        className="btn btn-sm btn-secondary text-white uppercase"
                     >
                         Book Appointment
                     </label>
