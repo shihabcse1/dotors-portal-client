1084bc1e66975cd0b21e3425629efe5da7644b00
shihabcse1 <tomdshihab@gmail.com>
2022-09-10 14:47:02 +0600

    Update profile code added

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -3,19 +3,27 @@ import { useForm } from "react-hook-form";
 import {
     useCreateUserWithEmailAndPassword,
     useSignInWithGoogle,
+    useUpdateProfile,
 } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] =
         useSignInWithGoogle(auth);
     const [createUserWithEmailAndPassword, user, loading, error] =
         useCreateUserWithEmailAndPassword(auth);
-    const onSubmit = (data) => {
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (data) => {
         console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log("update completed");
+        navigate("/appointment");
     };
     const {
         register,
@@ -25,14 +33,16 @@ const SignUp = () => {
 
     let signInError;
 
-    if (loading || gLoading) {
+    if (loading || gLoading || updating) {
         return <Loading></Loading>;
     }
 
-    if (error || gError) {
+    if (error || gError || updateError) {
         signInError = (
             <p className="text-red-500">
-                <small>{error?.message || gError?.message}</small>
+                <small>
+                    {error?.message || gError?.message || updateError?.message}
+                </small>
             </p>
         );
     }
