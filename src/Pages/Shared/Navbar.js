3c141c1ded0e95c80c220ad31bb9e270422f2b4d
shihabcse1 <tomdshihab@gmail.com>
2022-09-10 14:59:36 +0600

    SignOut code added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,7 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const logout = () => {
+        signOut(auth);
+    };
+
     const menuItems = (
         <>
             <li>
@@ -20,7 +29,13 @@ const Navbar = () => {
                 <Link to="/about">About</Link>
             </li>
             <li>
-                <Link to="/login">Login</Link>
+                {user ? (
+                    <button class="btn btn-ghost" onClick={logout}>
+                        Sign Out
+                    </button>
+                ) : (
+                    <Link to="/login">Login</Link>
+                )}
             </li>
         </>
     );
