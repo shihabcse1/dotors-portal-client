5db86a35d5f1f600a843339e559cfc9bf54e9d31
shihabcse1 <tomdshihab@gmail.com>
2022-09-24 11:28:06 +0600

    dashboard nested route added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -28,6 +28,11 @@ const Navbar = () => {
             <li>
                 <Link to="/about">About</Link>
             </li>
+            {user && (
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+            )}
             <li>
                 {user ? (
                     <button class="btn btn-ghost" onClick={logout}>
