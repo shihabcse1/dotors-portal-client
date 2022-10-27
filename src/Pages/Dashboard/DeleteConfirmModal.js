8018d57274a933b618da3ced0fb8bb08f284c3b2
shihabcse1 <tomdshihab@gmail.com>
2022-10-27 11:36:05 +0600

    DeleteConfirmModal Component added

---
+++ src/Pages/Dashboard/DeleteConfirmModal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DeleteConfirmModal = () => {
+    return (
+        <div>
+            <h1>Text</h1>
+        </div>
+    );
+};
+
+export default DeleteConfirmModal;
