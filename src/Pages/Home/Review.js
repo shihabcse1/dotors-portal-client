3f6c06e2a8ee6430056384119b4d98d1f2923f81
shihabcse1 <tomdshihab@gmail.com>
2022-10-27 14:13:27 +0600

    Dummy review data added for review page

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,11 +4,7 @@ const Review = ({ review }) => {
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Voluptatem quod, sequi aliquid consequuntur molestias
-                    praesentium!
-                </p>
+                <p>{review.review}</p>
                 <div className="flex items-center">
                     <div className="avatar">
                         <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
